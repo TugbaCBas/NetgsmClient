11115845f5cd70f35848b97f73fa611f7229dcec
Tugba Canoglu <taasnem.jano@ogr.iu.edu.tr>
2021-06-02 14:37:17 +0300

    fixed some issues

--- src/main/java/com/tugba/NetgsmClient/App.java
+++ src/main/java/com/tugba/NetgsmClient/App.java
@@ -1,13 +1,20 @@
 package com.tugba.NetgsmClient;
 
+import java.io.IOException;
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException, InterruptedException, ExecutionException
     {
-        System.out.println( "Hello World!" );
+        Client client = new Client();
+        client.sendMessage("hello its me");
+       RequestGenerator generator = new RequestGenerator();
+       System.out.println(" hello") ;
     }
 }
